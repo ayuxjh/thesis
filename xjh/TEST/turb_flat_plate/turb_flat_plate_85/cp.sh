cfba4d6cff0a12587ae61d8dad3893751b64a768
ayuxjh <18813008897@163.com>
2020-02-25 23:13:39 +0800

    chapter 4 2D case

---
+++ xjh/TEST/turb_flat_plate/turb_flat_plate_85/cp.sh
@@ -0,0 +1,7 @@
+cp -r ../coarse/Test_matrix/FGMRES_ILU0_85 ./coarse/FGMRES_ILU0_85
+cp -r ../coarse/Test_matrix/FGMRES_ILU1_85 ./coarse/FGMRES_ILU1_85
+cp -r ../medium/Test_matrix/FGMRES_ILU0_85 ./medium/FGMRES_ILU0_85
+cp -r ../medium/Test_matrix/FGMRES_ILU1_85 ./medium/FGMRES_ILU1_85
+cp -r ../fine_replace/Test_matrix/FGMRES_ILU0_85 ./fine_replace/FGMRES_ILU0_85
+cp -r ../fine_replace/Test_matrix/FGMRES_ILU1_85 ./fine_replace/FGMRES_ILU1_85
+
